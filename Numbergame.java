7b82b99bf9f9e70b799b46dfd0c9921a8b34115d
mohitmesh20 <163876408+mohitmesh20@users.noreply.github.com>
2024-03-18 23:28:12 +0530

    Add files via upload

---
+++ Numbergame.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Numbergame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int lowerBound = 1;
+            int upperBound = 100;
+            int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Welcome to the Number Guessing Game!");
+            System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
+
+            while (!guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                attempts++;
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You've guessed the correct number in " + attempts + " attempts!");
+                    guessedCorrectly = true;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainResponse = scanner.next().toLowerCase();
+            playAgain = playAgainResponse.equals("yes");
+        }
+
+        System.out.println("Thanks for playing!");
+        scanner.close();
+    }
+}
+
